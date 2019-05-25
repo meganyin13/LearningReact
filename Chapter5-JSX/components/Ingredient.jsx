a0d52828d652e6561f25ffbea781689d55c81e1b
meganyin13 <my474@cornell.edu>
2019-05-25 16:09:51 -0400

    chapter 5

---
+++ Chapter5-JSX/components/Ingredient.jsx
@@ -0,0 +1,8 @@
+const Ingredient = ({ amount, measurement, name }) =>
+    <li>
+        <span className="amount">{amount}</span>
+        <span className="measurement">{measurement}</span>
+        <span className="measurement">{measurement}</span>
+    </li>
+
+export default Ingredient
